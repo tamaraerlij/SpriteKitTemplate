f3a569acc4e27a97f74b8097b0c4005fc10307af
Tamara <tamara.erlij@gmail.com>
2019-10-17 02:41:45 -0300

    Animacao do Background

--- spriteKitTemplate/GameScene.swift
+++ spriteKitTemplate/GameScene.swift
@@ -11,23 +11,59 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    // Criação das variáveis
     var bird = SKSpriteNode()
+    var bg = SKSpriteNode()
     
 
     override func didMove(to view: SKView) {
+        
+        let bgTexture = SKTexture(imageNamed: "bg.png")
+        
+        let moveBGAnimation = SKAction.move(by: CGVector(dx: -bgTexture.size().width, dy: 0), duration: 7)
+        let shiftBGAnimation = SKAction.move(by: CGVector(dx: bgTexture.size().width, dy: 0), duration: 5)
+        let moveBGForever = SKAction.repeatForever(SKAction.sequence([moveBGAnimation, shiftBGAnimation]))
+        
+        var i : CGFloat = 0
+        
+        while i < 3 {
+
+               bg = SKSpriteNode(texture: bgTexture)
+               
+               bg.position = CGPoint(x: bgTexture.size().width / 2 + bgTexture.size().width * i , y: self.frame.midY)
+        
+               bg.size.height = self.frame.height
+        
+                bg.run(moveBGForever)
+               
+                 bg.zPosition = -1
+               self.addChild(bg)
+        
+                 i += 1
+            
+       
+    }
+        
+        // Criação da textura
         let birdTexture = SKTexture(imageNamed: "flappy1.png")
         let birdTexture2 = SKTexture(imageNamed: "flappy2.png")
         
+        // Pequena animação
         let animation = SKAction.animate(with: [birdTexture, birdTexture2], timePerFrame: 0.1)
         let makeBirdFlap = SKAction.repeatForever(animation)
         
+        // Incorporação da textura
         bird = SKSpriteNode(texture: birdTexture)
         
+        // posição do elemento
         bird.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
-        
         bird.run(makeBirdFlap)
-        
+        // Aparição
         self.addChild(bird)
+        
+       
+        
+        
     }
     
 
