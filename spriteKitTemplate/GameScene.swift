9d670e15507ba896ede7bf79f4b126556dbd0b17
Tamara <tamara.erlij@gmail.com>
2019-10-20 15:01:50 -0300

    score

--- spriteKitTemplate/GameScene.swift
+++ spriteKitTemplate/GameScene.swift
@@ -14,16 +14,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     // Criação das variáveis
     var bird = SKSpriteNode()
     var bg = SKSpriteNode()
+    var scoreLabel = SKLabelNode()
+    var score = 0
+    var gameOverLabel = SKLabelNode()
+    
     
     enum ColliderType: UInt32{
         case Bird = 1
         case Object = 2
+        case Gap = 4
         
     }
     var gameOver = false
     var backgroundMusic: SKAudioNode!
     @objc func makePipes() {
         let movePipes = SKAction.move(by: CGVector(dx: -2 * self.frame.width, dy: 0), duration: TimeInterval(self.frame.width / 100))
+        let removePipes = SKAction.removeFromParent()
+        let moveAndRemovePipes = SKAction.sequence([movePipes, removePipes])
               
               let gapHeight = bird.size.height * 4
               let movementAmount  = arc4random() % UInt32(self.frame.height / 2)
@@ -33,7 +40,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
               let pipeTexture = SKTexture(imageNamed: "pipe2.png")
               let pipe1 = SKSpriteNode(texture: pipeTexture)
               pipe1.position = CGPoint(x: self.frame.midX + self.frame.width, y: self.frame.midY + pipeTexture.size().height / 2 + gapHeight + pipeOffSet)
-              pipe1.run(movePipes)
+              pipe1.run(moveAndRemovePipes)
         pipe1.physicsBody = SKPhysicsBody(rectangleOf: pipeTexture.size())
         pipe1.physicsBody!.isDynamic = false
         pipe1.physicsBody!.contactTestBitMask = ColliderType.Object.rawValue
@@ -45,7 +52,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
               let pipe2Texture = SKTexture(imageNamed: "pipe2.png")
               let pipe2 = SKSpriteNode(texture: pipe2Texture)
               pipe2.position = CGPoint(x : self.frame.midX + self.frame.width, y: self.frame.midY - pipe2Texture.size().height / 2 - gapHeight - pipeOffSet)
-              pipe2.run(movePipes)
+              pipe2.run(moveAndRemovePipes)
         
         pipe2.physicsBody = SKPhysicsBody(rectangleOf: pipeTexture.size())
         pipe2.physicsBody!.isDynamic = false
@@ -54,27 +61,44 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         pipe2.physicsBody!.collisionBitMask = ColliderType.Bird.rawValue
         
               self.addChild(pipe2)
+      
+        let gap = SKNode()
+        gap.position = CGPoint(x: self.frame.midX + self.frame.width, y: self.frame.midY + pipeOffSet )
+        gap.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: pipeTexture.size().width, height: gapHeight))
+        gap.physicsBody?.isDynamic = false
+        gap.run(moveAndRemovePipes)
+        gap.physicsBody!.contactTestBitMask = ColliderType.Bird.rawValue
+        gap.physicsBody!.categoryBitMask = ColliderType.Gap.rawValue
+        gap.physicsBody!.collisionBitMask = ColliderType.Gap.rawValue
         
+        self.addChild(gap)
     }
     func didBegin(_ contact: SKPhysicsContact) {
+        if contact.bodyA.categoryBitMask == ColliderType.Gap.rawValue || contact.bodyA.categoryBitMask == ColliderType.Gap.rawValue {
+           score += 1
+            scoreLabel.text = String(score)
+            
+        } else {
         self.speed = 0
         gameOver = true
+            gameOverLabel.fontName = "Helvetica"
+            gameOverLabel.fontSize = 30
+            gameOverLabel.text = "Game over! tap to play again"
+            gameOverLabel.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+            
+            self.addChild(gameOverLabel)
     }
-
+ }
     override func didMove(to view: SKView ) {
-        
         self.physicsWorld.contactDelegate = self
         Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(self.makePipes), userInfo: nil, repeats: true)
-     
         // Adicionar música de fundo
         if let musicURL = Bundle.main.url(forResource: "BackgroundMusic", withExtension: ".mp3") {
         backgroundMusic = SKAudioNode(url: musicURL)
         addChild(backgroundMusic)
         }
         
-        
         let bgTexture = SKTexture(imageNamed: "bg.png")
-        
         let moveBGAnimation = SKAction.move(by: CGVector(dx: -bgTexture.size().width, dy: 0), duration: 7)
         let shiftBGAnimation = SKAction.move(by: CGVector(dx: bgTexture.size().width, dy: 0), duration: 5)
         let moveBGForever = SKAction.repeatForever(SKAction.sequence([moveBGAnimation, shiftBGAnimation]))
@@ -137,6 +161,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         ground.physicsBody!.collisionBitMask = ColliderType.Bird.rawValue
         
         self.addChild(ground)
+        scoreLabel.fontName = "Helvetica"
+        scoreLabel.fontSize = 60
+        scoreLabel.text = "0"
+        scoreLabel.position = CGPoint(x: self.frame.midX, y: self.frame.height / 2 - 70)
+        self.addChild(scoreLabel)
     }
     
 
@@ -148,7 +177,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
            bird.physicsBody!.velocity = CGVector(dx: 0, dy: 0)
         
-           bird.physicsBody!.applyImpulse(CGVector(dx: 0, dy: 100)) 
+           bird.physicsBody!.applyImpulse(CGVector(dx: 0, dy: 100))
      
         }
         }
